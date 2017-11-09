ec9b5dd9c30770d95adb09a22bf372b9b211037b
Adeline <adelinedidier@hotmail.com>
2017-11-09 16:11:55 +0100

    succes figaro

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -10,10 +10,10 @@ class SendTweet
 
 	def log_in_to_twitter
 		@client = Twitter::REST::Client.new do |config|
-  	config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
-  	config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
-  	config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
-  	config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
+  	config.consumer_key        = Figaro.env.YOUR_CONSUMER_KEY
+  	config.consumer_secret     = Figaro.env.YOUR_CONSUMER_SECRET
+  	config.access_token        = Figaro.env.YOUR_ACCESS_TOKEN
+  	config.access_token_secret = Figaro.env.YOUR_ACCESS_SECRET
 		end
 	end
 
